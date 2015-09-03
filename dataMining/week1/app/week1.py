4dd663cbbf513c3981831a5b71b83a47fda18fff
Ir1sh <mmoore@iofficecorp.com>
2015-09-03 15:27:29 -0500

    added comments and print statements for final question

--- dataMining/week1/app/week1.py
+++ dataMining/week1/app/week1.py
@@ -37,27 +37,35 @@ class CoinToss():
         heads = 0
         tails = 0
         tossed = 0
-
+        #toss the coin the number of times requested
         for i in range(number): #range has start defaulted to 0
             result = self.toss()
+            #decide if it was heads or tails
             if(result == 0):
                 heads = heads + 1
             else:
                 tails = tails + 1
-
+            #increase the counter for the number of tosses completed
             tossed = tossed + 1
+            #do the requested action if it is at the requested interval ie 100 % 100 = 0 therefore do the action
             if(tossed % doEvery == 0):
                 doSomething([heads, tails])
+        #return a list with two items the number of heads and the number of tails
         return [heads, tails]
 
     def getProportion(self, a, b):
+        #get proportion as a float
         return float(a) / float(b)
 
     def questionOne(self):
         def doThis(res):
+            #we were asked to print the proportion of the rsults minus 1/2, 1/2 => 0.5
             print(self.getProportion(res[0], res[1]) - 0.5)
 
         def nowDoThis(res):
+            #for part two we need to print the proportion of results minus 1/2 AND
+            #print the number of heads less half the number of tosses
+            # this number approaches zero because as N increases the number of heads results will approach 50%
             doThis(res)
             print(res[0] - 2500)
 
@@ -70,24 +78,27 @@ class CoinToss():
 
     def questionTwo(self):
         results = [0, 0]
+        # this function returns true if the proportion of toss results is within the
+        #parameters given by the question ie p is within 0.1 of 0.5
         def isWithinParams(res):
             p = self.getProportion(res[0], res[1]) - 0.5
             return p > 0.4 and p < 0.6 
 
         def doThis(res):
-            print(res)
+            #record the results of each run of the experiement
             if(isWithinParams(res)):
                 results[0] = results[0] + 1
             else:
                 results[1] = results[1] + 1
 
+        #run the experiment 100 times
         for i in range(100):
-            self.customTosser(800, doThis, 800)
+            self.customTosser(1500, doThis, 1500)
         
 
         print('the proportion of results within our params was :')
         print(results[0], results[1])
-        print(self.getProportion(results[0], results[1]))
+        print('N needs to be approximately 1200 - 1500 to so that approx 95 out of 100 times the proportion of heads is between .4 and .6')
 
 
 
@@ -95,10 +106,10 @@ class CoinToss():
 def run():
     print('Mark Moore solutions for CS-583 - Week 1')
     print('Informatics Problem 6.14.5:') 
-    #print('When you slice the given string after the colon and convert to a float you get %s' % StringQuestions().convertToFloat())
+    print('When you slice the given string after the colon and convert to a float you get %s' % StringQuestions().convertToFloat())
     print('Informatics Problem 7.11.1:')
-    #print('The required file in uppcase is:')
-    #StringQuestions().convertFileToUpperCase('./app/mobox-short.txt')
+    print('The required file in uppercase is:')
+    StringQuestions().convertFileToUpperCase('./app/mobox-short.txt')
     print('Probability Page 12, Problem 1')
     CoinToss().questionOne()
     print('Probability Page 12, Problem 2')
